c51a2263e5cc89767a615ede5ac90f696e0d5a1f
luissian <chapado.l@gmail.com>
2022-01-18 09:34:08 +0100

    working on export table to excel

--- aicespana/static/aicespana/js/aicespana.js
+++ aicespana/static/aicespana/js/aicespana.js
@@ -1,7 +1,7 @@
 // Download a table to excel //
 function exportTableToExcel(tableID, filename = ''){
     var downloadLink;
-    var dataType = 'application/vnd.ms-excel;charset=UTF-8';
+    var dataType = 'application/vnd.ms-excel';
     var tableSelect = document.getElementById(tableID);
     var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
 
