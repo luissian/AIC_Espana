27b1fb3dbf1bcd29675754c7ffe14df6b417e3a3
luissian <chapado.l@gmail.com>
2022-05-20 11:49:41 +0200

    remove debuging

--- aicespana/views.py
+++ aicespana/views.py
@@ -515,7 +515,6 @@ def modificacion_voluntario(request):
 
         for item in field_list:
             data[item] = request.POST[item]
-        import pdb; pdb.set_trace()
         user_obj.update_all_data_for_voluntary(data)
 
         return render(request,'aicespana/modificacionVoluntario.html',{'confirmation_data':request.POST['nombre'] + ' ' + request.POST['apellidos']})
